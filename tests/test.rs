f200f2f655a873954211037ad33aaeebd7530613
gipsyh <gipsyh.icu@gmail.com>
2021-12-06 14:23:41 +0000

    Rewrite test frame

--- tests/test.rs
+++ tests/test.rs
@@ -1,56 +1,60 @@
-use async_rdma::{QueuePairEndpoint, Rdma, RdmaBuilder, RdmaLocalBox, RdmaRemoteBox};
-use std::{
-    io,
-    net::{TcpStream, ToSocketAddrs},
-    thread::spawn,
-};
+use async_rdma::{Rdma, RdmaListener};
+use futures::Future;
+use tokio::{io, net::ToSocketAddrs};
 
-fn server<A: ToSocketAddrs>(addr: A, f: fn(Rdma, TcpStream) -> io::Result<()>) -> io::Result<()> {
-    let rdma = RdmaBuilder::default().build()?;
-    let (stream, _) = std::net::TcpListener::bind(addr)?.accept()?;
-    let remote: QueuePairEndpoint = bincode::deserialize_from(&stream).unwrap();
-    bincode::serialize_into(&stream, &rdma.endpoint()).unwrap();
-    rdma.handshake(remote)?;
-    std::thread::sleep(std::time::Duration::from_millis(100));
-    f(rdma, stream)
-}
+type RdmaFn<R> = fn(Rdma) -> R;
 
-fn client<A: ToSocketAddrs>(addr: A, f: fn(Rdma, TcpStream) -> io::Result<()>) -> io::Result<()> {
-    let rdma = RdmaBuilder::default().build()?;
-    let stream = std::net::TcpStream::connect(addr).unwrap();
-    bincode::serialize_into(&stream, &rdma.endpoint()).unwrap();
-    let remote: QueuePairEndpoint = bincode::deserialize_from(&stream).unwrap();
-    rdma.handshake(remote)?;
-    std::thread::sleep(std::time::Duration::from_millis(100));
-    f(rdma, stream)
-}
-
-fn test_server_client<A: 'static + ToSocketAddrs + Send + Copy>(
+#[tokio::main]
+async fn server<A: ToSocketAddrs, R: Future<Output = Result<(), io::Error>>>(
     addr: A,
-    s: fn(Rdma, TcpStream) -> io::Result<()>,
-    c: fn(Rdma, TcpStream) -> io::Result<()>,
+    f: RdmaFn<R>,
 ) -> io::Result<()> {
-    let server = spawn(move || server(addr, s));
-    std::thread::sleep(std::time::Duration::from_millis(100));
-    let client = spawn(move || client(addr, c));
+    let rdma = RdmaListener::bind(addr).await?.accept().await?;
+    f(rdma).await
+}
+
+#[tokio::main]
+async fn client<A: ToSocketAddrs, R: Future<Output = Result<(), io::Error>>>(
+    addr: A,
+    f: RdmaFn<R>,
+) -> io::Result<()> {
+    let rdma = Rdma::connect(addr).await?;
+    f(rdma).await
+}
+
+fn test_server_client<
+    A: 'static + ToSocketAddrs + Send + Copy,
+    SR: Future<Output = Result<(), io::Error>> + 'static,
+    CR: Future<Output = Result<(), io::Error>> + 'static,
+>(
+    addr: A,
+    s: RdmaFn<SR>,
+    c: RdmaFn<CR>,
+) -> io::Result<()> {
+    let server = std::thread::spawn(move || server(addr, s));
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    let client = std::thread::spawn(move || client(addr, c));
     client.join().unwrap()?;
     server.join().unwrap()
 }
 
 mod test1 {
     use crate::*;
+    use async_rdma::RdmaMemory;
+    use std::{alloc::Layout, sync::Arc};
 
-    fn server(rdma: Rdma, _stream: TcpStream) -> io::Result<()> {
-        let data: RdmaLocalBox<[u32; 4]> = rdma.post_receive()?;
-        std::thread::sleep(std::time::Duration::from_millis(1000));
-        assert_eq!(*data, [4, 3, 2, 1]);
+    async fn server(rdma: Rdma) -> io::Result<()> {
+        let mr = rdma.receive_mr().await.unwrap();
+        dbg!(unsafe { *(mr.addr() as *mut i32) });
         Ok(())
     }
 
-    fn client(rdma: Rdma, _stream: TcpStream) -> io::Result<()> {
-        let data = RdmaLocalBox::new(&rdma.pd, [4, 3, 2, 1]);
-        rdma.post_send(&data)?;
-        std::thread::sleep(std::time::Duration::from_millis(1000));
+    async fn client(rdma: Rdma) -> io::Result<()> {
+        let rmr = Arc::new(rdma.alloc_remote_mr(Layout::new::<i32>()).await.unwrap());
+        let lmr = rdma.alloc_local_mr(Layout::new::<i32>()).unwrap();
+        unsafe { *(lmr.addr() as *mut i32) = 5 };
+        rdma.write(&lmr, rmr.as_ref()).await.unwrap();
+        rdma.send_mr(rmr.clone()).await.unwrap();
         Ok(())
     }
 
@@ -58,64 +62,4 @@ mod test1 {
     fn test() -> io::Result<()> {
         test_server_client("127.0.0.1:8000", server, client)
     }
-}
-
-mod test2 {
-    use crate::*;
-
-    fn server(rdma: Rdma, stream: TcpStream) -> io::Result<()> {
-        let local_box = RdmaLocalBox::new(&rdma.pd, [1, 2, 3, 4]);
-        let remote_box: RdmaRemoteBox = bincode::deserialize_from(&stream).unwrap();
-        rdma.qp.write(&local_box, &remote_box, 0)
-    }
-
-    fn client(rdma: Rdma, stream: TcpStream) -> io::Result<()> {
-        let local_box = RdmaLocalBox::new(&rdma.pd, [0, 0, 0, 0]);
-        bincode::serialize_into(&stream, &local_box.remote_box()).unwrap();
-        std::thread::sleep(std::time::Duration::from_millis(500));
-        assert_eq!(*local_box, [1, 2, 3, 4]);
-        Ok(())
-    }
-
-    #[test]
-    fn test() -> io::Result<()> {
-        test_server_client("127.0.0.1:8001", server, client)
-    }
-}
-
-mod test3 {
-    use crate::*;
-
-    async fn server(rdma: Rdma, stream: TcpStream) -> io::Result<()> {
-        let mut local_box = RdmaLocalBox::new(&rdma.pd, [0, 0, 0, 0]);
-        let remote_box: RdmaRemoteBox = bincode::deserialize_from(&stream).unwrap();
-        rdma.read(&mut local_box, &remote_box).await?;
-        std::thread::sleep(std::time::Duration::from_millis(500));
-        assert_eq!(*local_box, [1, 2, 3, 4]);
-        Ok(())
-    }
-
-    fn client(rdma: Rdma, stream: TcpStream) -> io::Result<()> {
-        let local_box = RdmaLocalBox::new(&rdma.pd, [1, 2, 3, 4]);
-        bincode::serialize_into(&stream, &local_box.remote_box()).unwrap();
-        std::thread::sleep(std::time::Duration::from_millis(500));
-        Ok(())
-    }
-
-    #[test]
-    fn test() -> io::Result<()> {
-        test_server_client("127.0.0.1:8002", server, client)
-    }
-}
-
-#[test]
-fn test_loopback() -> io::Result<()> {
-    let rdma = RdmaBuilder::default().build()?;
-    rdma.handshake(rdma.endpoint())?;
-    let box1 = RdmaLocalBox::new(&rdma.pd, [0, 0, 0, 0]);
-    let box2 = RdmaLocalBox::new(&rdma.pd, [1, 2, 3, 4]);
-    rdma.write(&box2, &box1.remote_box())?;
-    std::thread::sleep(std::time::Duration::from_millis(500));
-    assert_eq!(*box1, [1, 2, 3, 4]);
-    Ok(())
 }
